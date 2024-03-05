d0b6a1c048c08ecd4684deb61d2882d4b4d5ca1d
Evan Glas <eglas27@gmail.com>
2024-03-05 00:48:10 -0500

    Updated

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch("data.json")
     .then((response) => response.json())
     .then((data) => {
+      const root = document.documentElement;
+      root.style.setProperty("--primary-color", data.themeColor);
+      root.style.setProperty("--highlight-color", data.highlightColor);
+
       document.getElementById("name").textContent = data.profile.name;
       document.getElementById("profileImage").src = data.profile.image;
       document.getElementById("resumeLink").href = data.profile.resume;
       document.getElementById("linkedinLink").href = data.profile.linkedin;
+      document.getElementById("email").href = data.profile.email;
       document.getElementById("aboutContent").textContent = data.aboutMe;
 
       const educationList = document.getElementById("educationList");
@@ -14,11 +19,13 @@ document.addEventListener("DOMContentLoaded", function () {
         li.innerHTML = `<strong>${item.degree}</strong>`;
         let ul = document.createElement("ul");
         let school = document.createElement("li");
-        school.innerHTML = `${item.school}`;
+        school.innerHTML = `<i>${item.school}</i>`;
         ul.appendChild(school);
-        let year = document.createElement("li");
-        year.innerHTML = `${item.year}`;
-        ul.appendChild(year);
+        item.description.forEach((desc) => {
+          let description = document.createElement("li");
+          description.innerHTML = `${desc}`;
+          ul.appendChild(description);
+        });
         li.appendChild(ul);
         educationList.appendChild(li);
       });
@@ -26,27 +33,41 @@ document.addEventListener("DOMContentLoaded", function () {
       const experienceList = document.getElementById("experienceList");
       data.experience.forEach((item) => {
         let li = document.createElement("li");
-        li.innerHTML = `
-    <strong>${item.role}</strong>
-    <ul>
-      <li>${item.company}</li>
-      <li>${item.year}</li>
-      <li>${item.description}</li>
-    </ul>
-  `;
+        let ul = document.createElement("ul");
+        li.innerHTML = `<strong>${item.role}</strong>`;
+        let company = document.createElement("li");
+        company.innerHTML = `<i>${item.organization}</i>`;
+        ul.appendChild(company);
+
+        item.description.forEach((desc) => {
+          let description = document.createElement("li");
+          description.innerHTML = `${desc}`;
+          ul.appendChild(description);
+        });
+        li.appendChild(ul);
         experienceList.appendChild(li);
       });
 
       const projectsList = document.getElementById("projectsList");
       data.projects.forEach((item) => {
         let li = document.createElement("li");
-        li.innerHTML = `
-    <strong>${item.title}</strong>: ${item.description} 
-    <br>
-    <div style="text-align: center; margin-bottom: 20px; margin-top: 20px;">
-      <img class="project-image" src="${item.image}" alt="${item.title}" style="max-width: 500px; border-radius: 10px;">
-    </div>
-  `;
+        if (item.link != "") {
+          li.innerHTML = `
+            <strong>${item.title}</strong>: ${item.description} 
+            <br>
+            <div style="text-align: center; margin-bottom: 20px; margin-top: 20px;">
+              <a href="${item.link}"><img class="project-image" src="${item.image}" alt="${item.title}" style="max-width: 500px; border-radius: 10px;"></a>
+            </div>
+          `;
+        } else {
+          li.innerHTML = `
+          <strong>${item.title}</strong>: ${item.description} 
+          <br>
+          <div style="text-align: center; margin-bottom: 20px; margin-top: 20px;">
+          <img class="project-image" src="${item.image}" alt="${item.title}" style="max-width: 500px; border-radius: 10px;">
+          </div>
+        `;
+        }
         projectsList.appendChild(li);
       });
 
